441768c9dbb7a31bf6c613f2c9e50c728e9057d7
BerenSa <sanchezesther108@gmail.com>
2026-02-12 19:32:32 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -54,4 +54,9 @@ def delete(id):
     con.close()
     return {"ok":True}
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
